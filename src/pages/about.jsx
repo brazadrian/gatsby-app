d8d59cb92bd62b28063f11f5e61c65f8625a0480
Adrian de Souza <60609409+brazadrian@users.noreply.github.com>
2023-05-23 11:43:15 -0300

    bootstrap react

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 
 import CardV2 from "../components/cardV2"
+import BasicExample from "../components/basicExample"
 
 const About = () => {
     var text = "About Page"
@@ -12,6 +13,7 @@ const About = () => {
             <h2>About 2</h2>
             <p>Send us a message!</p>
             <p>{text}</p>
+            <BasicExample />
 
             <p>
                 Link externo (do google): <a href="https://www.google.com/" blank>Clique aqui</a>
