97a07a3f7feb627443eaa972bd62b3dba0bf0687
Adrian de Souza <60609409+brazadrian@users.noreply.github.com>
2023-05-27 07:49:26 -0300

    consumindo api rest dos sensores

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-rest-api",
+      options: {
+        endpoints: ["https://back-people.onrender.com/sensor"],
+      },
+    },
   ],
 }
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,41 +3,37 @@ import { useStaticQuery, graphql } from "gatsby"
 /* import * as Styled from "./styled" */
 
 const Footer = () => {
-    const {
-        site: {
-            siteMetadata: {
-                copyright,
-                siteUrl,
-                /* description,
-                title, */
-            }
-        }
-    } = useStaticQuery(graphql`
+  const {
+    site: {
+      siteMetadata: {
+        copyright,
+        siteUrl,
+      },
+    },
+  } = useStaticQuery(graphql`
     {
-        site {
-            siteMetadata {
-              copyright
-              description
-              siteUrl
-              title
-            }
+      site {
+        siteMetadata {
+          copyright
+          siteUrl
         }
+      }
     }
-    `)
+  `)
 
-    return (
-        <div>
-            <footer className="bg-light text-center text-lg-start">
-                <div className="text-center p-3">
-                    <p>
-                        <a href={siteUrl} target="_blank" rel="noreferrer noopener">
-                            {copyright}
-                        </a>
-                    </p>
-                </div>
-            </footer >
-        </div >
-    )
+  return (
+    <div>
+      <footer className="bg-light text-center text-lg-start">
+        <div className="text-center p-3">
+          <p>
+            <a href={siteUrl} target="_blank" rel="noreferrer noopener">
+              {copyright}
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  )
 }
 
 export default Footer
